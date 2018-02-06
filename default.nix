82fb7ca80a98c6a148d2c54cfc671d0e0b53975f
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2018-02-07 02:54:50 +0900

    Add console versions and delete -with-mods derivations

--- default.nix
+++ default.nix
@@ -6,12 +6,9 @@ with self;
   inherit (super.callPackage ./lib {}) wrapCDDA buildCDDAMod;
 
   cataclysm-dda = wrapCDDA super.cataclysm-dda { mods = []; };
-
-  cataclysm-dda-with-mods = mods: wrapCDDA super.cataclysm-dda { mods = mods; };
-
+  cataclysm-dda-console = wrapCDDA super.cataclysm-dda { tiles = false; mods = []; };
   cataclysm-dda-git = wrapCDDA super.cataclysm-dda-git { mods = []; };
-
-  cataclysm-dda-git-with-mods = mods: wrapCDDA super.cataclysm-dda-git { mods = mods; };
+  cataclysm-dda-git-console = wrapCDDA super.cataclysm-dda-git { tiles = false; mods = []; };
 
   cataclysmDDAMods = {
   };
