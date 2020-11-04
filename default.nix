a1eec3dd1aea024819d08bdc71152d678d6f23f5
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2020-11-04 19:37:19 +0900

    Rewrite default.nix

--- default.nix
+++ default.nix
@@ -1,54 +1,57 @@
 self: super:
 
-with self;
-
 let
-  withMorePkgsFor = build: newPkgs: oldPkgs:
-  lib.recursiveUpdate oldPkgs (filterAvailablePkgsFor build {
-    mod = {
-    };
+  inherit (super) lib;
 
+  oldPkgs = {
+    inherit (super.cataclysmDDA.pkgs) mod soundpack tileset;
+  };
+
+  newPkgs = {
     soundpack = super.callPackage ./soundpacks.nix {};
+  };
 
-    tileset = {
-    };
-  });
-
-  addMorePkgsTo = build:
+  updatePkgs = build:
   let
     self = build.overrideAttrs (old: {
       passthru = old.passthru // {
-        pkgs = old.passthru.pkgs.extend (withMorePkgsFor self);
-        withMods = cataclysmDDA.wrapCDDA self;
+        pkgs = pkgsFor self;
+        withMods = super.cataclysmDDA.wrapCDDA self;
       };
     });
   in
   self;
 
-  filterAvailablePkgsFor = build: pkgs:
-  lib.mapAttrs (_: mods: lib.filterAttrs (availableFor build) mods) pkgs;
+  pkgsFor = build:
+  let
+    pkgs = lib.recursiveUpdate oldPkgs newPkgs;
+    pkgs' = lib.mapAttrs (_: mods: lib.filterAttrs (availableFor build) mods) pkgs;
+  in
+  lib.makeExtensible (_: pkgs');
 
   availableFor = build: _: mod:
   if isNull build then
     true
   else if build.isTiles then
-    mod.forTiles
+    mod.forTiles or false
+  else if build.isCurses then
+    mod.forCurses or false
   else
-    mod.forCurses;
+    false;
 in
 
 {
   cataclysmDDA = super.cataclysmDDA // (with super.cataclysmDDA; {
-    pkgs = pkgs.extend (withMorePkgsFor null);
+    pkgs = pkgsFor null;
 
     stable = {
-      tiles = addMorePkgsTo stable.tiles;
-      curses = addMorePkgsTo stable.curses;
+      tiles = updatePkgs stable.tiles;
+      curses = updatePkgs stable.curses;
     };
 
     git = {
-      tiles = addMorePkgsTo git.tiles;
-      curses = addMorePkgsTo git.curses;
+      tiles = updatePkgs git.tiles;
+      curses = updatePkgs git.curses;
     };
   });
 }
