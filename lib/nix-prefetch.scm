a7563b2ffe84ec676d169860458b6d0d344f2569
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2020-11-09 12:29:25 +0900

    Lock sha256-cache while updating

--- lib/nix-prefetch.scm
+++ lib/nix-prefetch.scm
@@ -1,4 +1,5 @@
 (define-module nix-prefetch
+  (use file.util)
   (use gauche.parameter)
   (use gauche.process)
   (use rfc.json)
@@ -10,15 +11,18 @@
 (define sha256-cache-path (make-parameter ".sha256-cache.json"))
 
 (define (nix-prefetch-url/cache url :key (unpack? #f) (name #f))
-  (let1 cache (or (parameterize ([json-object-handler (cut alist->hash-table <> 'string=?)])
-                    (with-input-from-file (sha256-cache-path) (cut parse-json)))
-                  (make-hash-table 'string=?))
-    (or (ref cache url #f)
-        (let1 sha256 (process-output->string
-                       `(nix-prefetch-url ,@(if unpack? '(--unpack) '())
-                                          ,@(if name `(--name ,name) '())
-                                          ,url))
-          (begin
-            (set! (~ cache url) sha256)
-            (with-output-to-file (sha256-cache-path) (cut construct-json cache))
-            sha256)))))
+  (with-lock-file
+    ".lock-sha256-cache"
+    (lambda ()
+      (let1 cache (or (parameterize ([json-object-handler (cut alist->hash-table <> 'string=?)])
+                        (with-input-from-file (sha256-cache-path) (cut parse-json)))
+                      (make-hash-table 'string=?))
+        (or (ref cache url #f)
+            (let1 sha256 (process-output->string
+                           `(nix-prefetch-url ,@(if unpack? '(--unpack) '())
+                                              ,@(if name `(--name ,name) '())
+                                              ,url))
+              (begin
+                (set! (~ cache url) sha256)
+                (with-output-to-file (sha256-cache-path) (cut construct-json cache))
+                sha256)))))))
