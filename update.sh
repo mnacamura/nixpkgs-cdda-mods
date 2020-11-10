1121c16bc2a49cdcacde61d63c13db36ea803a4b
Mitsuhiro Nakamura <m.nacamura@gmail.com>
2020-11-10 10:46:09 +0900

    Refactor the script for automatic update

--- update.sh
+++ update.sh
@@ -1,43 +1,93 @@
 #!/usr/bin/env bash
 
-echo -n "Updating jenkins builds..." >&2
+set -euo pipefail
+
+remote_name=github
+remote_branch=master
+commit_msg="Automatic overlay update"
+
+create_commit=yes
+deploy_update=no
+
+msg() {
+    echo -e "\e[32m[MSG]\e[0m $*" >&2
+}
+
+warn() {
+    echo -e "\e[33m[WRN]\e[0m $*" >&2
+}
+
+err() {
+    echo -e "\e[31m[ERR]\e[0m $*" >&2
+    exit 1
+}
+
+show_usage() {
+    echo "Usage: $0 [--no-commit] [--deploy] [-h|--help]" >&2
+}
+
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        --no-commit)
+            create_commit=no
+            shift
+            ;;
+        --deploy)
+            deploy_update=yes
+            shift
+            ;;
+        -h|--help)
+            show_usage
+            exit 1
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+if [ "$create_commit" = no -a "$deploy_update" = yes ]; then
+    err "Are you sure to deploy update without creating any commit?"
+fi
+
+msg "Updating jenkins builds..."
 if ! ./update-jenkins.scm; then
-    echo "...failed" >&2
-    exit 1
+    err "...failed"
 fi
-echo "...done" >&2
+msg "...done"
 
-echo -n "Updating mods..." >&2
+msg "Updating mods..."
 if ! ./update-mods.scm; then
-    echo "...failed" >&2
-    exit 1
+    err "...failed"
 fi
-echo "...done" >&2
+msg "...done"
 
-if [ "$(git diff ./generated | wc -l)" -eq 0 ]; then
-    echo "No updates found" >&2
+if [ "$(git diff ./generated .sha256-cache.json | wc -l)" -eq 0 ]; then
+    msg "No updates found"
     exit 0
 else
-    echo "Updates found" >&2
+    msg "Updates found"
 fi
 
-echo -n "Staging the updates..." >&2
-if ! git add ./generated .sha256-cache.json; then
-    echo "...failed" >&2
-    exit 1
-fi
-echo "...done"
+if [ "$create_commit" = yes ]; then
+    msg "Staging the updates..."
+    if ! git add ./generated .sha256-cache.json; then
+        err "...failed"
+    fi
+    msg "...done"
 
-echo -n "Creating a git commit..." >&2
-if ! git commit -m "Update overlay"; then
-    echo "...failed" >&2
-    exit 1
+    msg "Creating a git commit..."
+    if ! git commit -m "$commit_msg"; then
+        err "...failed"
+    fi
+    msg "...done"
 fi
-echo "...done"
 
-echo -n "Pushing the git commit..." >&2
-if ! git push github master; then
-    echo "...failed" >&2
-    exit 1
+
+if [ "$deploy_update" = yes ]; then
+    msg "Pushing the git commit..."
+    if ! git push "$remote_name" "$remote_branch"; then
+        err "...failed"
+    fi
+    msg "...done"
 fi
-echo "...done"
